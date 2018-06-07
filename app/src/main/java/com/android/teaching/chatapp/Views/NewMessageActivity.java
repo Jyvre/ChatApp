395e933501a8479d4e6e71d7e1694a98d2f9a811
CYSTORBOOKV2\Javier <thej96fa@gmail.com>
2018-06-07 12:34:42 +0200

    v0.5.0
    Done 3.c with String methods and editText

--- app/src/main/java/com/android/teaching/chatapp/Views/NewMessageActivity.java
+++ app/src/main/java/com/android/teaching/chatapp/Views/NewMessageActivity.java
@@ -5,17 +5,24 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.EditText;
+
 import com.android.teaching.chatapp.R;
 
 
 public class NewMessageActivity extends AppCompatActivity {
 
     private Toolbar toolbar;
+    private EditText user;
+    private EditText message;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate( savedInstanceState );
         setContentView( R.layout.activity_new_message );
+
+        user = findViewById( R.id.editTextUSR );
+        message = findViewById( R.id.toolbarMSG );
 
         toolbar = findViewById( R.id.toolbarMSG );
         toolbar.setTitle( getResources().getString( R.string.anewmessage ) );
@@ -27,6 +34,12 @@ public class NewMessageActivity extends AppCompatActivity {
     }
 
     public void onClickEnviar(View view) {
-
+        if(user.getText().toString().isEmpty()){
+            user.setError( getResources().getString( R.string.errorUser ) );
+        }else if(message.getText().toString().isEmpty()){
+            message.setError( getResources().getString( R.string.errorMsg ) );
+        }else{
+            
+        }
     }
 }
